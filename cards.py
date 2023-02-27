b84495c37114f1a00cff5a4b6a833b28c07b54cf
vub44 <86915047+vub44@users.noreply.github.com>
2023-02-27 17:59:46 +0100

    updated the code, slightly better

--- cards.py
+++ cards.py
@@ -23,37 +23,54 @@ class Deck:
 
     def convert(self, card):
         # order: spades, hearts, clubs, diamonds
-        types = ["♤", "♥", "♧", "♦"]
+        types = ["♠", "♡", "♣", "♢"]
 
-        if card in self.hearts:
-            return f"{types[0]}{card % 13}"
-        if card in self.spades:
-            return f"{types[1]}{card % 13}"
-        if card in self.diamonds:
-            return f"{types[2]}{card % 13}"
-        if card in self.clubs:
-            return f"{types[3]}{card % 13}"
+        if type(card) == type(1):
+            if card > 0:
+                if card < 53:
+                    if card in self.hearts:
+                        return f"{types[0]}{card % 13}"
+                    if card in self.spades:
+                        return f"{types[1]}{card % 13}"
+                    if card in self.diamonds:
+                        return f"{types[2]}{card % 13}"
+                    if card in self.clubs:
+                        return f"{types[3]}{card % 13}"
+                else:
+                    return "not a card"
+            else:
+                return "not a card"
+        else:
+            return "not a card"
 
 
-    def draw(self, amount):
+
+
+    def draw(self, amount, convert):
         drawn = []
         if amount > len(self.deck):
             amount = len(self.deck)
         for i in range(1, amount+1):
             drawn.append(self.deck.pop(0))
 
-        final = []
 
-        for i in drawn:
-            final.append(self.convert(i))
+        if convert:
+            final = []
+            for i in drawn:
+                final.append(self.convert(i))
 
-        return final
+            return final
+        else:
+            return drawn
 
 
-    def add(self, card):
+    def add(self, card, shuffled):
         if type(card) == type(1):
             if card > 0 and card < 53:
-                self.deck.append(card)
+                if shuffled:
+                    self.deck.insert(card, random.randrange(1,52))
+                else:
+                    self.deck.insert(card, 0)
             else:
                 return "not a card"
         else:
